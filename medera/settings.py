a1bdb867d5d22d6db7943917df8aa6c553a7010c
unknown <paulius9902@gmail.com>
2022-02-18 20:15:45 +0200

    -

--- medera/settings.py
+++ medera/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','tasktrackerapps.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
